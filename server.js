977aa8d6957ccc883c30846c4da1a1ca5a406603
Preeti Gupta <preetigupta510@gmail.com>
2020-08-07 22:45:49 -0700

    correcting the url for heroku mongodb deploy

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-//mongoose.connect(process.env.MONGODB_URI|| "mongodb://localhost/workout",{useNewUrlParser:true})
+
+
+mongoose.connect(process.env.MONGODB_URI|| "mongodb://localhost/workout",{useNewUrlParser:true})
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
